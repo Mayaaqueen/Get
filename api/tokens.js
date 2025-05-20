a6b2f545567fb808436c32ad380eefd2162bfcb6
Mayaaqueen <honorofkingsyusuf@gmail.com>
2025-05-20 08:38:26 +0700

    Update tokens.js
    Apis

--- api/tokens.js
+++ api/tokens.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch("http://68.183.229.2:3000/tokens.json");
+  const response = await fetch("https://api.datworld.fun/tokens.json");
   const data = await response.json();
   res.status(200).json(data);
 }
