90e18cd3ee0fd3f70647a8a8f39b113279e3fef0
Mayaaqueen <honorofkingsyusuf@gmail.com>
2025-05-24 02:12:21 +0700

    Delete api directory

--- api/tokens.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch("https://api.datworld.fun/tokens.json");
-  const data = await response.json();
-  res.status(200).json(data);
-}
